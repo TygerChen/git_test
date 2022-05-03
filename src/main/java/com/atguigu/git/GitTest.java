78ab58451b8900d9b2d6173b9898df9f1ec8bcb5
TygerChen <TygerChen@qq.com>
2022-05-03 10:42:58 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author Tyger Chen
+ * @Date 2022/5/3 10:39
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
